cfe041f6d639b11e181f619d8a949f91f27b9a89
Tim Heckman <timothy.heckman@gmail.com>
2011-12-23 05:30:05 -0500

    add logging subroutine to clean up other code

--- he-ipv4.pl
+++ he-ipv4.pl
@@ -3,6 +3,7 @@
 use 5.10.1;
 use warnings;
 use strict;
+use Switch;
 
 use YAML::Tiny;
 use HTTP::Request;
@@ -22,26 +23,43 @@ our $configFile = "/var/cache/he-ipv4.yml";
 our $curUser = getlogin();
 
 if ($curUser ne "root") {
-	if ($debug >= 1) { error("the IPv4 update script must be executed by root, not " . $curUser . ". exiting"); }
-	if ($debug == 4) { say("the IPv4 update script must be executed by root, not " . $curUser . ". exiting"); }
+	slog("the IPv4 update script must be executed by root, not " . $curUser . ". exiting", 1)
 	exit 1;
 }
 
 undef $curUser;
 
 unless (-e $configFile) {
-	if ($debug >= 3) { info("\"/var/cache/he-ipv4.yml\" doesn't exist. attempting to create file"); }
-	if ($debug == 4) { say("\"/var/cache/he-ipv4.yml\" doesn't exist. attempting to create file"); }
+	slog("\"/var/cache/he-ipv4.yml\" doesn't exist. attempting to create file", 3);
+	slog("woooo", 3);
 	my $yaml = YAML::Tiny->new;
 	$yaml->[0]->{ipv4} = '127.0.0.1';
 	$yaml->[0]->{url} = '0';
 	$yaml->write($configFile);
 	if (-e $configFile) {
-		if ($debug >= 3) { info("file created successfully"); }
-		if ($debug == 4) { say("file created successfully"); }
+		slog("file created successfully", 3);
 	} else {
-		if ($debug >= 3) { error("crap, something didn't go as planned. file does not appear to have been created. exiting"); }
-		if ($debug == 4) { say("crap, something didn't go as planned. file does not appear to have been created. exiting"); }
+		slog("crap, something didn't go as planned. file does not appear to have been created. exiting", 1);
 		exit 1;
+	}
+}
+
+sub slog {
+	if ($debug >= 1) {
+		my $message = shift;
+		my $level = shift;
+		switch ($level) {
+			case 3 {
+				if ($level <= $debug) { info($message); }
+			}
+			case 2 {
+				if ($level <= $debug) { warning($message); }
+			}
+			case 1 {
+				if ($level <= $debug) { error($message); }
+			}
+			else { warning("incorrect value used for message level on subroutine slog call on line " . __LINE__); }
+		}
+		if ($debug == 4) { say($message); }
 	}
 }
